ecd0190848ef720d9d2868fc1774a2523150c524
Vittor Patricio <vittorpatricio26@gmail.com>
2025-02-11 23:02:21 -0300

    estilo na tab

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,14 +1,39 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'green' , headerShown: false} }>
+    <Tabs
+  screenOptions={{
+    tabBarActiveTintColor: '#EAAE00',
+    headerShown: false,
+    tabBarShowLabel: false, // Remove os textos
+    tabBarStyle: {
+      paddingHorizontal: '5%',
+      borderTopRightRadius: 15,
+      borderTopLeftRadius: 15,
+      backgroundColor: '#606060',
+      height: 86,
+      overflow: 'hidden',
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      elevation: 4,
+      flexDirection: 'row',
+      justifyContent: 'space-evenly', // Centraliza os itens na TabBar
+      alignItems: 'center',
+    },
+  }}
+>
+
+
       <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+         title: 'Home',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="target" size={32} color={color} />,
         }}
       />
 
@@ -16,29 +41,21 @@ export default function TabLayout() {
         name="profile"
         options={{
           title: 'Configurações',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="gear" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={32} name="gear" color={color} />,
         }}
       />
       <Tabs.Screen
         name="stats"
         options={{
           title: 'Ranking',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="star" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={32} name="star" color={color} />,
         }}
       />
             <Tabs.Screen
         name="ia"
         options={{
           title: 'Chat',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="star" color={color} />,
-        }}
-      />
-
-<Tabs.Screen
-        name="stats"
-        options={{
-          title: 'stats',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={32} name="star" color={color} />,
         }}
       />
     </Tabs>
